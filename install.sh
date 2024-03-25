2c9dbba92aba9a26d08994ccb454b6e87832df0b
Sifat Shaharia <49650283+0sifat@users.noreply.github.com>
2024-03-25 15:45:52 +0600

    compose update

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ sudo apt install docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ubuntu
 docker run hello-world
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
